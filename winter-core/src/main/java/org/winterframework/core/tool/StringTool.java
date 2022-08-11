09b3a1f4fd7dd44f91331b37804e54cad06459c1
qinglinl <qinglinl@rings.net.cn>
2022-08-11 10:25:58 +0800

    StringTool增加方法

--- winter-core/src/main/java/org/winterframework/core/tool/StringTool.java
+++ winter-core/src/main/java/org/winterframework/core/tool/StringTool.java
@@ -8,4 +8,68 @@ import org.apache.commons.lang3.StringUtils;
  */
 public final class StringTool extends StringUtils {
 	private StringTool() {}
+
+	public static Integer convert2Int(Object o, Integer defaultVal) {
+		return convertNumber(o, defaultVal, () -> Integer.parseInt(o.toString()));
+	}
+
+	public static int convert2Int(Object o) {
+		return convert2Int(o, 0);
+	}
+
+	public static Long convert2Long(Object o, Long defaultVal) {
+		return convertNumber(o, defaultVal, () -> Long.parseLong(o.toString()));
+	}
+
+	public static long convert2Long(Object o) {
+		return convert2Long(o, 0L);
+	}
+
+	public static Double convert2Double(Object o, Double defaultVal) {
+		return convertNumber(o, defaultVal, () -> Double.parseDouble(o.toString()));
+	}
+
+	public static double convert2Double(Object o) {
+		return convert2Double(o, 0.0);
+	}
+
+	public static Float convert2Float(Object o, Float defaultVal) {
+		return convertNumber(o, defaultVal, () -> Float.parseFloat(o.toString()));
+	}
+
+	public static float convert2Float(Object o) {
+		return convert2Float(o, 0.0f);
+	}
+
+	public static Byte convert2Byte(Object o, Byte defaultVal) {
+		return convertNumber(o, defaultVal, () -> Byte.parseByte(o.toString()));
+	}
+
+	public static byte convert2Byte(Object o) {
+		return convert2Byte(o, (byte) 0);
+	}
+
+	public static Short convert2Short(Object o, Short defaultVal) {
+		return convertNumber(o, defaultVal, () -> Short.valueOf(o.toString()));
+	}
+
+	public static short convert2Short(Object o) {
+		return convert2Short(o, (short) 0);
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <T extends Number> T convertNumber(Object o, T defaultVal, ConvertCallback<T> convertCallback) {
+		if (o == null) {
+			return defaultVal;
+		}
+		if (defaultVal.getClass().isInstance(o)) {
+			return (T) o;
+		}
+		return convertCallback.invoke();
+	}
+
+	@FunctionalInterface
+	public interface ConvertCallback<T> {
+		T invoke();
+	}
 }
