4a73ac9215fdf8e68f71274364aa60804bca9f9a
Sven <sven199109@gmail.com>
2023-07-09 14:39:02 +0800

    打印启动日志trace id

--- winter-starter-tracer/src/main/java/org/winterframework/trace/configuration/WinterTraceAutoConfiguration.java
+++ winter-starter-tracer/src/main/java/org/winterframework/trace/configuration/WinterTraceAutoConfiguration.java
@@ -1,15 +1,23 @@
 package org.winterframework.trace.configuration;
 
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplicationRunListener;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.winterframework.trace.constant.TraceConstants;
 import org.winterframework.trace.interceptor.WinterRestTemplateInterceptor;
 import org.winterframework.trace.interceptor.WinterTracerInterceptor;
+import org.winterframework.trace.tool.MDCTool;
+
+import java.time.Duration;
 
 /**
  * @author sven
@@ -17,13 +25,24 @@ import org.winterframework.trace.interceptor.WinterTracerInterceptor;
  */
 @Configuration
 @ComponentScan(basePackages = "org.winterframework.trace")
-public class WinterTraceAutoConfiguration implements WebMvcConfigurer {
+@Slf4j
+public class WinterTraceAutoConfiguration implements WebMvcConfigurer, SpringApplicationRunListener {
     @Autowired
     private WinterTracerInterceptor winterTracerInterceptor;
     @Autowired
     private WinterRestTemplateInterceptor restTemplateInterceptor;
     @Autowired
     private RestTemplate restTemplate;
+
+    @Override
+    public void contextPrepared(ConfigurableApplicationContext context) {
+        MDC.put(TraceConstants.TRACE_KEY, MDCTool.uuid());
+    }
+
+    @Override
+    public void ready(ConfigurableApplicationContext context, Duration timeTaken) {
+        MDC.clear();
+    }
 
     @PostConstruct
     public void init() {
