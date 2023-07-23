d5e65029efa8c995385467124782e0251ca8ff9a
Sven <sven199109@gmail.com>
2023-07-23 11:16:33 +0800

    修改启动脚本

--- winter-admin/winter-admin-web/bin/start.sh
+++ winter-admin/winter-admin-web/bin/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 java \
-  -Xloggc:./gc.log -XX:+PrintGCDetails \
-  -Xms=128m -Xmx=1G -Xss=128m -Xmn=128m -XX:MetaspaceSize=256M \
+  -Xlog:gc:./gc.log -XX:+PrintGCDetails \
+  -Xms128m -Xmx1G -Xss128m -Xmn128m -XX:MetaspaceSize=256M \
   -jar target/winter-admin-web-0.0.1-SNAPSHOT.jar
