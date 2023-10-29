d7ba0fad8fe1f495999fe988fe010cb3ba604cfa
qinglin.liu <qinglin.liu@transsnet.com>
2023-10-29 23:12:32 +0800

    增加概率随机方法

--- winter-core/src/main/java/org/winterframework/core/tool/RandomTool.java
+++ winter-core/src/main/java/org/winterframework/core/tool/RandomTool.java
@@ -9,4 +9,23 @@ import lombok.experimental.UtilityClass;
  */
 @UtilityClass
 public class RandomTool extends RandomUtil {
+    /**
+     * 根据机率随机获取
+     * @param probArr 概率列表
+     * @return 对应的下标
+     */
+    public static int randomPick(double[] probArr) {
+        double total = 0;
+        for (double p : probArr) {
+            total += p;
+        }
+        double random = Math.random() * total;
+        for (int i = 0; i < probArr.length; i++) {
+            random -= probArr[i];
+            if (random <= 0) {
+                return i;
+            }
+        }
+        return 0;
+    }
 }
