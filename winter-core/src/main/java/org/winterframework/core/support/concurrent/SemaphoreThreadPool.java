33dd1b3c9a5bf93f4bca686c5900b336577f9448
qinglin.liu <qinglin.liu@transsnet.com>
2024-08-21 08:22:58 +0800

    实现应用shutdown时自动清理线程池

--- winter-core/src/main/java/org/winterframework/core/support/concurrent/SemaphoreThreadPool.java
+++ winter-core/src/main/java/org/winterframework/core/support/concurrent/SemaphoreThreadPool.java
@@ -1,6 +1,9 @@
 package org.winterframework.core.support.concurrent;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.lang.NonNull;
 
 import java.util.concurrent.*;
 
@@ -10,7 +13,7 @@ import java.util.concurrent.*;
  * Created on 2022/3/2 10:30 上午
  */
 @Slf4j
-public class SemaphoreThreadPool {
+public class SemaphoreThreadPool implements ApplicationListener<ContextClosedEvent> {
     private final Semaphore semaphore;
 
     private final ThreadPoolExecutor pool;
@@ -86,5 +89,21 @@ public class SemaphoreThreadPool {
      */
     public void releaseAll() {
         semaphore.release(threadCount);
+    }
+
+    @Override
+    public void onApplicationEvent(@NonNull ContextClosedEvent event) {
+        log.info("应用程序关闭，正在清理线程池...");
+        // 关闭时清理线程池
+        pool.shutdown();
+        try {
+            // 等待线程池结束
+            if (!pool.awaitTermination(10, TimeUnit.SECONDS)) {
+                pool.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            pool.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
     }
 }
